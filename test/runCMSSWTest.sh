ff5c4037dad1f53f8d3b16b5d649c9b15e3ae82b
daiktas <vilius.cepaitis@gmail.com>
2020-01-31 11:10:05 +0000

    up test

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -6,23 +6,16 @@ function run_test()
     cd ~
     source ~/.bashrc
     export SCRAM_ARCH=slc6_amd64_gcc630 || return 1
-    scramv1 project CMSSW CMSSW_10_2_0_pre2 || return 1
-    cd CMSSW_10_2_0_pre2/src || return 1
+    scramv1 project CMSSW CMSSW_10_2_18 || return 1
+    cd CMSSW_10_2_18/src || return 1
     eval `scramv1 runtime -sh` || return 1
     mkdir -p PhysicsTools/NanoAODTools
     rsync -r --stats /scripts/ PhysicsTools/NanoAODTools/. || return 1
     scram b || return 1
      
     echo "--- Test evaluation script ---"
-    wget -nv https://github.com/LLPDNNX/test-files/raw/master/pbmodel/model_ctau1.pb || return 1
-    wget -nv https://github.com/LLPDNNX/test-files/raw/master/pbmodel/model_parametric.pb || return 1
-    wget -nv https://github.com/LLPDNNX/test-files/raw/master/pbmodel/feature_dict.py || return 1
-    mv feature_dict.py PhysicsTools/NanoAODTools/test/LLP/.
     python PhysicsTools/NanoAODTools/test/LLP/testEval.py --input=https://github.com/LLPDNNX/test-files/raw/master/nanoaod/RunIISummer16NanoAODv2_MC.root . || return 1
 
-    echo "--- Test new module chain ---"
-    python PhysicsTools/NanoAODTools/test/LLP/processSR.py --isData --input=https://github.com/LLPDNNX/test-files/raw/master/nanoaod/RunIISummer16NanoAODv2_MC.root . || return 1
-    python PhysicsTools/NanoAODTools/test/LLP/processMuonCR.py --input=https://github.com/LLPDNNX/test-files/raw/master/nanoaod/RunIISummer16NanoAODv2_MC.root . || return 1
 }
 
 
