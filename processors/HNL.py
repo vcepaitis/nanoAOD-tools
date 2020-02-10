59f52507aee4768c1eb6b7838a68101a30a140a1
Haifa Rejeb Sfar <haifa.rejeb.sfar@cern.ch>
2020-02-10 11:47:08 +0100

    adding met variables

--- processors/HNL.py
+++ processors/HNL.py
@@ -14,71 +14,47 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.modules import *
 
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('--isData', dest='isData', action='store_true',default=False)
-#parser.add_argument('--year', dest='year', action='store',type=int, default=2016)
-#parser.add_argument('--input', dest='inputFiles', action='append',default=[])
-#parser.add_argument('output', nargs=1)
+parser = argparse.ArgumentParser()
+parser.add_argument('--isData', dest='isData', action='store_true',default=False)
+parser.add_argument('--year', dest='year', action='store',type=int, default=2016)
+parser.add_argument('--input', dest='inputFiles', action='append',default=[])
+parser.add_argument('output', nargs=1)
 
-#args = parser.parse_args()
-#print "isData:",args.isData
-#print "year:",args.year
-#print "inputs:",len(args.inputFiles)
+args = parser.parse_args()
 
-isData = False
-year=2016
-input_filename = sys.argv[1]
-year  = sys.argv[2]
-isData = sys.argv[3]
-outpout = sys.argv[5]
-os.environ["X509_USER_PROXY"] = sys.argv[4]
-outpout = sys.argv[5]
-print os.environ["X509_USER_PROXY"]
-
-#output 
-
-
-#for inputFile in args.inputFiles:
-print ("The name of the file is ", input_filename)
-
-input_file = open(input_filename, 'r')
-for File in input_file:
-
-   FilesVector = []
-   File = File.rstrip()
-   print "file name is : ", File  , "\n"
-   FilesVector.append(File)
-   rootFile = ROOT.TFile.Open(File)
-   if not rootFile:
-	print "yes same issue",rootFile
-        print "CRITICAL - file '"+str(input_file)+"' not found!"
+print "isData:",args.isData
+print "year:",args.year
+print "inputs:",len(args.inputFiles)
+for inputFile in args.inputFiles:
+    rootFile = ROOT.TFile.Open(inputFile)
+    if not rootFile:
+        print "CRITICAL - file '"+inputFile+"' not found!"
         sys.exit(1)
-   tree = rootFile.Get("Events")
-   if not tree:
-     #  print "CRITICAL - 'Events' tree not found in file '"+inputFile+"'!"
+    tree = rootFile.Get("Events")
+    if not tree:
+        print "CRITICAL - 'Events' tree not found in file '"+inputFile+"'!"
         sys.exit(1)
-   #print " - ",inputFile,", events=",tree.GetEntries()
-   #print "output directory:",args.output[0]
+    print " - ",inputFile,", events=",tree.GetEntries()
+    
+print "output directory:",args.output[0]
 
-   globalOptions = {
-      "isData":isData,
-      "year":2016
-   }
+globalOptions = {
+    "isData":args.isData,
+    "year":args.year
+}
 
 
-   muonSelection = [
-      MuonSelection(
+muonSelection = [
+    MuonSelection(
         outputName="tightMuons",
         storeKinematics=['pt','eta', 'dxy', 'dxyErr', 'dz', 'dzErr', 'phi'],
         storeWeights=True,
         muonMinPt = 25.,
-        muonMaxDxy = 0.002,
-        muonMaxDz = 0.01,
         muonID = MuonSelection.TIGHT,
         muonIso = MuonSelection.TIGHT,
         globalOptions=globalOptions
-     ),
-     MuonSelection(
+    ),
+    MuonSelection(
         inputCollection = lambda event: event.tightMuons_unselected,
         outputName="looseMuons",
         storeKinematics=['pt','eta', 'dxy', 'dxyErr', 'dz', 'dzErr', 'phi'],
@@ -87,116 +63,121 @@ for File in input_file:
         muonID = MuonSelection.LOOSE,
         muonIso = MuonSelection.NONE,
         globalOptions=globalOptions
-     ),
+    ),
     
-     SingleMuonTriggerSelection(
+    SingleMuonTriggerSelection(
         inputCollection=lambda event: event["tightMuons"],
         outputName="IsoMuTrigger",
         storeWeights=False,
         globalOptions=globalOptions
-     ),
-     EventSkim(selection=lambda event: event.IsoMuTrigger_flag==1),
-     EventSkim(selection=lambda event: event.ntightMuons>0),
-     EventSkim(selection=lambda event: event.nlooseMuons>0)
-   ]
+    ),
+    EventSkim(selection=lambda event: event.IsoMuTrigger_flag==1),
+    EventSkim(selection=lambda event: event.ntightMuons==1),
+    EventSkim(selection=lambda event: event.nlooseMuons==1)
+]
 
-   analyzerChain = []
+analyzerChain = []
 
-   analyzerChain.extend(muonSelection)
+analyzerChain.extend(muonSelection)
 
-   analyzerChain.append(
-      JetSelection(
-      )
-   )
-
-
-   storeVariables = [
-      [lambda tree: tree.branch("genweight","F"),lambda tree,event: tree.fillBranch("genweight",event.Generator_weight)],
-   ]
+analyzerChain.append(
+    JetSelection(
+    )
+)
 
 
-   analyzerChain.append(EventInfo(storeVariables=storeVariables))
-
-   analyzerChain.append(EventSkim(selection=lambda event: len(event.selectedJets)>0))
+analyzerChain.append(EventSkim(selection=lambda event: len(event.selectedJets)>0))
 
 
-   analyzerChain.append(
-     InvariantSystem(
+analyzerChain.append(
+    InvariantSystem(
         inputCollection = lambda event: [event.looseMuons[0], event.tightMuons[0]],
         outputName = "dimuon"
-     )
-   )
+    )
+)
 
-   analyzerChain.append(
-     LepJetFinder(
+analyzerChain.append(
+    LepJetFinder(
         jetCollection = lambda event: event.selectedJets,
         leptonCollection = lambda event: event.looseMuons,
-     )
-   )
- 
-   analyzerChain.append(
-     InvariantSystem(
-        inputCollection = lambda event: [event.tightMuons[0], event.lepJet[0]],
-        outputName = "lepjet_muon"
-     )
-   )
-
-   analyzerChain.append( 
+    )
+)
+analyzerChain.append( 
      MetFilter(
        outputName ="MET_filter"
     )
-   )
+)
 
-   analyzerChain.append(
+analyzerChain.append(
      EventObservables(
        jetCollection = lambda event: event.selectedJets,
        leptonCollection = lambda event: event.tightMuons[0] 
      )
+) 
 
 
-   ) 
-
-   analyzerChain.append(
-     TaggerEvaluation(
+  
+analyzerChain.append(
+    EventSkim(selection=lambda event: 
+        event.dimuon_deltaR > 1 and event.dimuon_deltaR < 5,
+    )
+)
+   
+analyzerChain.append(
+    TaggerEvaluation(
         modelPath="PhysicsTools/NanoAODTools/data/nn/weight2016_75.pb",
         logctauValues = [1.74],
         inputCollections=[
-            lambda event: event.lepJet
+            lambda event: event.lepJet,
+            lambda event: event.selectedJets     
         ],
         taggerName="llpdnnx",
-     )
-   )
+    )
+)
 
 
-   analyzerChain.append(
-     JetTaggerResult(
+analyzerChain.append(
+    JetTaggerResult(
         inputCollection = lambda event: event.lepJet,
         taggerName = "llpdnnx",
         outputName = "lepJet",
         logctauValues = [1.74],
-        predictionLabels = ["LLP_Q", "LLP_QMU"],
-     )
-   )
+	predictionLabels = ["LLP_Q", "LLP_QMU"],
+    )
+)
 
 
-   analyzerChain.append(
-     JetTruthFlags(inputCollection= lambda event: event.selectedJets,
+analyzerChain.append(
+    JetTruthFlags(inputCollection= lambda event: event.selectedJets,
         outputName="selectedJets"
-     )
-   )
+    )
+)
 
-   analyzerChain.append(
-     JetTruthFlags(inputCollection= lambda event: event.lepJet,
+analyzerChain.append(
+    JetTruthFlags(inputCollection= lambda event: event.lepJet,
         outputName="lepJet"
-     )
-   )
+    )
+)
+
+analyzerChain.append(
+    TaggerWorkingpoints(
+        inputCollection = lambda event: event.selectedJets,
+    )
+)
 
 
-   p=PostProcessor(
-    sys.argv[5],
-    [FilesVector],
+storeVariables = [
+    [lambda tree: tree.branch("genweight","F"),lambda tree,event: tree.fillBranch("genweight",event.Generator_weight)],
+]
+
+
+analyzerChain.append(EventInfo(storeVariables=storeVariables))
+
+p=PostProcessor(
+    args.output[0],
+    [args.inputFiles],
     modules=analyzerChain,
     maxEvents=-1,
     friend=True
-   )
-   p.run()
+)
+p.run()
