2e35f58c8b4ed43762fde303e26c7b5fef3f4842
daiktas <vilius.cepaitis@gmail.com>
2020-03-25 15:10:50 +0000

    lep jet

--- processors/HNL.py
+++ processors/HNL.py
@@ -5,28 +5,28 @@ import json
 import argparse
 import random
 import ROOT
-ROOT.PyConfig.IgnoreCommandLineOptions = True
 from importlib import import_module
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.modules import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2       import * 
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetRecalib import *
 
+ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--isData', dest='isData', action='store_true',default=False)
-parser.add_argument('--year', dest='year', action='store',type=int, default=2016)
-parser.add_argument('--input', dest='inputFiles', action='append',default=[])
+parser.add_argument('--isData', dest='isData', action='store_true', default=False)
+parser.add_argument('--year', dest='year', action='store', type=int, default=2016)
+parser.add_argument('--input', dest='inputFiles', action='append', default=[])
 parser.add_argument('output', nargs=1)
 
 args = parser.parse_args()
 
 
-print "isData:",args.isData
-print "inputs:",len(args.inputFiles)
+print "isData:", args.isData
+print "inputs:", len(args.inputFiles)
 for inputFile in args.inputFiles:
     print "2018" in inputFile
     if "2016" in inputFile:
@@ -45,14 +45,14 @@ for inputFile in args.inputFiles:
     if not tree:
         print "CRITICAL - 'Events' tree not found in file '"+inputFile+"'!"
         sys.exit(1)
-    print " - ",inputFile,", events=",tree.GetEntries()
-    
-print "year:",year
-print "output directory:",args.output[0]
+    print " - ", inputFile, ", events=", tree.GetEntries()
+
+print "year:", year
+print "output directory:", args.output[0]
 
 globalOptions = {
-    "isData":args.isData,
-    "year":year
+    "isData": args.isData,
+    "year": year
 }
 
 isMC = not args.isData
@@ -60,41 +60,43 @@ isMC = not args.isData
 minMuonPt = {2016: 25., 2017: 28., 2018: 25.}
 
 muonSelection = [
-    EventSkim(selection=lambda event: event.nTrigObj>0),
+    EventSkim(selection=lambda event: event.nTrigObj > 0),
     MuonSelection(
         outputName="tightMuon",
-        storeKinematics=['pt','eta', 'dxy', 'dxyErr', 'dz', 'dzErr', 'phi', 'pfRelIso04_all'],
+        storeKinematics=['pt', 'eta', 'dxy', 'dxyErr', 'dz',
+                         'dzErr', 'phi', 'pfRelIso04_all'],
         storeWeights=True,
-        muonMinPt = minMuonPt[globalOptions["year"]],
-        triggerMatch = True,
-        muonID = MuonSelection.TIGHT,
-        muonIso = MuonSelection.TIGHT,
+        muonMinPt=minMuonPt[globalOptions["year"]],
+        triggerMatch=True,
+        muonID=MuonSelection.TIGHT,
+        muonIso=MuonSelection.TIGHT,
         selectLeadingOnly=True,
         globalOptions=globalOptions
     ),
-    EventSkim(selection=lambda event: event.ntightMuon==1),
+    EventSkim(selection=lambda event: event.ntightMuon == 1),
     MuonSelection(
-        inputCollection = lambda event: event.tightMuon_unselected,
+        inputCollection=lambda event: event.tightMuon_unselected,
         outputName="looseMuons",
-        storeKinematics=['pt','eta', 'dxy', 'dxyErr', 'dz', 'dzErr', 'phi', 'pfRelIso04_all', 'tightId'],
+        storeKinematics=['pt', 'eta', 'dxy', 'dxyErr', 'dz',
+                         'dzErr', 'phi', 'pfRelIso04_all', 'tightId', "muon frac"],
         storeWeights=True,
-        muonMinPt = 5.,
-        muonID = MuonSelection.LOOSE,
-        muonIso = MuonSelection.NONE,
+        muonMinPt=5.,
+        muonID=MuonSelection.LOOSE,
+        muonIso=MuonSelection.NONE,
         globalOptions=globalOptions
     ),
-    
+
     SingleMuonTriggerSelection(
         inputCollection=lambda event: event.tightMuon,
         outputName="IsoMuTrigger",
         storeWeights=True,
         globalOptions=globalOptions
     ),
-    EventSkim(selection=lambda event: event.IsoMuTrigger_flag==1),
-    EventSkim(selection=lambda event: event.nlooseMuons>0)
+    EventSkim(selection=lambda event: event.IsoMuTrigger_flag == 1),
+    EventSkim(selection=lambda event: event.nlooseMuons > 0)
 ]
 
-#analyzerChain = [jetRecalibration]
+# analyzerChain = [jetRecalibration]
 analyzerChain = []
 
 analyzerChain.extend(muonSelection)
@@ -106,20 +108,50 @@ analyzerChain.append(
     )
 )
 
-analyzerChain.append(EventSkim(selection=lambda event: len(event.selectedJets)>0))
+analyzerChain.append(
+    EventSkim(
+        selection=lambda event: len(event.selectedJets) > 0
+    )
+)
 
 
 analyzerChain.append(
     InvariantSystem(
-        inputCollection = lambda event: [event.looseMuons[0], event.tightMuon[0]],
-        outputName = "dimuon"
+        inputCollection=lambda event: [event.looseMuons[0], event.tightMuon[0]],
+        outputName="dimuon"
     )
 )
 
 analyzerChain.append(
     LepJetFinder(
-        jetCollection = lambda event: event.selectedJets,
-        leptonCollection = lambda event: event.looseMuons,
+        jetCollection=lambda event: event.selectedJets,
+        leptonCollection=lambda event: event.looseMuons,
+    )
+)
+
+analyzerChain.append(
+    LepJetFinder(
+        jetCollection=lambda event: event.selectedJets,
+        leptonCollection=lambda event: event.looseMuons,
+        usePFLinking=False,
+        outputName="lepJet_old"
+    )
+)
+
+
+analyzerChain.append(
+    JetTruthFlags(
+        inputCollection=lambda event: event.lepJet,
+        outputName="lepJet",
+        globalOptions=globalOptions
+    )
+)
+
+analyzerChain.append(
+    JetTruthFlags(
+        inputCollection=lambda event: event.lepJet_old,
+        outputName="lepJet_old",
+        globalOptions=globalOptions
     )
 )
 
@@ -131,14 +163,14 @@ analyzerChain.append(
 '''
 
 modelPath = {2016: "PhysicsTools/NanoAODTools/data/nn/weight2016_75.pb",
-            2017: "PhysicsTools/NanoAODTools/data/nn/weight2017_68.pb",
-            2018: "PhysicsTools/NanoAODTools/data/nn/weight2018_73.pb"
-            }
+             2017: "PhysicsTools/NanoAODTools/data/nn/weight2017_68.pb",
+             2018: "PhysicsTools/NanoAODTools/data/nn/weight2018_73.pb"
+             }
 
 analyzerChain.append(
     TaggerEvaluation(
         modelPath=modelPath[year],
-        logctauValues = range(-1, 4),
+        logctauValues=range(-1, 4),
         inputCollections=[
             lambda event: event.lepJet,
         ],
@@ -146,39 +178,32 @@ analyzerChain.append(
     )
 )
 
-
 analyzerChain.append(
     JetTaggerResult(
-        inputCollection = lambda event: event.lepJet,
-        taggerName = "llpdnnx",
-        outputName = "lepJet",
-        logctauValues = range(-1, 4),
+        inputCollection=lambda event: event.lepJet,
+        taggerName="llpdnnx",
+        outputName="lepJet",
+        logctauValues=range(-1, 4),
     )
 )
 
 
 analyzerChain.append(
-    JetTruthFlags(inputCollection= lambda event: event.lepJet,
-        globalOptions=globalOptions,
-        outputName="lepJet"
-    )
-)
-
-
-analyzerChain.append( 
      MetFilter(
-     	globalOptions=globalOptions,
-     	outputName="MET_filter"
-     	)
-)
-analyzerChain.append(
-     EventObservables(
-       jetCollection = lambda event: event.selectedJets,
-       leptonCollection = lambda event: event.tightMuon[0] 
+        globalOptions=globalOptions,
+        outputName="MET_filter"
      )
 )
+
+analyzerChain.append(
+     EventObservables(
+       jetCollection=lambda event: event.selectedJets,
+       leptonCollection=lambda event: event.tightMuon[0]
+     )
+)
+
+
 '''
- 
 analyzerChain.append(
     XGBEvaluation(
         modelPath="PhysicsTools/NanoAODTools/data/nn/bdt.model",
@@ -187,33 +212,41 @@ analyzerChain.append(
 '''
 
 storeVariables = [
-    [lambda tree: tree.branch("MET_pt", "F"), lambda tree,event: tree.fillBranch("MET_pt", event.MET_pt)],
-    [lambda tree: tree.branch("MET_phi", "F"), lambda tree,event: tree.fillBranch("MET_phi", event.MET_phi)],
-    [lambda tree: tree.branch("MET_significance", "F"), lambda tree,event: tree.fillBranch("MET_significance", event.MET_significance)],
-    [lambda tree: tree.branch("PV_npvs", "I"), lambda tree,event: tree.fillBranch("PV_npvs", event.PV_npvs)],
-    [lambda tree: tree.branch("PV_npvsGood", "I"), lambda tree,event: tree.fillBranch("PV_npvsGood", event.PV_npvsGood)],
-    [lambda tree: tree.branch("fixedGridRhoFastjetAll", "F"), lambda tree,event: tree.fillBranch("fixedGridRhoFastjetAll", event.fixedGridRhoFastjetAll)],
+    [lambda tree: tree.branch("MET_pt", "F"), lambda tree,
+     event: tree.fillBranch("MET_pt", event.MET_pt)],
+    [lambda tree: tree.branch("MET_phi", "F"), lambda tree,
+     event: tree.fillBranch("MET_phi", event.MET_phi)],
+    [lambda tree: tree.branch("MET_significance", "F"), lambda tree,
+     event: tree.fillBranch("MET_significance", event.MET_significance)],
+    [lambda tree: tree.branch("PV_npvs", "I"), lambda tree,
+     event: tree.fillBranch("PV_npvs", event.PV_npvs)],
+    [lambda tree: tree.branch("PV_npvsGood", "I"), lambda tree,
+     event: tree.fillBranch("PV_npvsGood", event.PV_npvsGood)],
+    [lambda tree: tree.branch("fixedGridRhoFastjetAll", "F"), lambda tree,
+     event: tree.fillBranch("fixedGridRhoFastjetAll", event.fixedGridRhoFastjetAll)],
     #[lambda tree: tree.branch("bdt_score", "F"), lambda tree,event: tree.fillBranch("bdt_score", event.bdt_score)]
 ]
 
 if not globalOptions["isData"]:
-    storeVariables.append([lambda tree: tree.branch("genweight","F"),lambda tree,event: tree.fillBranch("genweight",event.Generator_weight)])
+    storeVariables.append([lambda tree: tree.branch("genweight", "F"), lambda tree,
+                           event: tree.fillBranch("genweight", event.Generator_weight)])
 
 
 analyzerChain.append(EventInfo(storeVariables=storeVariables))
 
 analyzerChain.append(
     PileupWeight(
-        outputName ="puweight",
+        outputName="puweight",
         globalOptions=globalOptions
     )
 )
 
-p=PostProcessor(
+p = PostProcessor(
     args.output[0],
     [args.inputFiles],
     modules=analyzerChain,
     maxEvents=-1,
     friend=True
 )
+
 p.run()
--- python/modules/EventObservables.py
+++ python/modules/EventObservables.py
@@ -8,17 +8,18 @@ import random
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
-from utils import deltaR,deltaPhi
+from utils import deltaR, deltaPhi
+
 
 class EventObservables(Module):
 
     def __init__(
         self,
-        jetCollection = lambda event: Collection(event, "Jet"),
-        leptonCollection = lambda event: Collection(event, "Muon"),
-        metInput = lambda event: Object(event, "MET"),
-        outputName = "Jet_Muon",
-        globalOptions={"isData":False}
+        jetCollection=lambda event: Collection(event, "Jet"),
+        leptonCollection=lambda event: Collection(event, "Muon"),
+        metInput=lambda event: Object(event, "MET"),
+        outputName="EventObservables",
+        globalOptions={"isData": False}
     ):
         self.globalOptions = globalOptions
         self.jetCollection = jetCollection
@@ -28,70 +29,68 @@ class EventObservables(Module):
 
     def beginJob(self):
         pass
-        
+
     def endJob(self):
         pass
-        
+
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-        self.out.branch(self.outputName+"_ht","F")
-        self.out.branch(self.outputName+"_mht","F")
-        self.out.branch(self.outputName+"_mht_phi","F")
-        self.out.branch(self.outputName+"_mass","F")
-        self.out.branch(self.outputName+"_minPhi","F")
-        
-        if self.leptonCollection!=None:
-            self.out.branch(self.outputName+"_MET_NoMu_pt","F")
-            self.out.branch(self.outputName+"_MET_NoMu_phi","F")
-            self.out.branch(self.outputName+"_MET_Mu_mT","F")
-            self.out.branch(self.outputName+"_mht_NoMu","F")
-        
+        self.out.branch(self.outputName+"_ht", "F")
+        self.out.branch(self.outputName+"_mht", "F")
+        self.out.branch(self.outputName+"_mht_phi", "F")
+        self.out.branch(self.outputName+"_mass", "F")
+        self.out.branch(self.outputName+"_minPhi", "F")
+
+        if self.leptonCollection is not None:
+            self.out.branch(self.outputName+"_MET_NoMu_pt", "F")
+            self.out.branch(self.outputName+"_MET_NoMu_phi", "F")
+            self.out.branch(self.outputName+"_MET_Mu_mT", "F")
+            self.out.branch(self.outputName+"_mht_NoMu", "F")
+
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
-        
+
     def analyze(self, event):
-        """process event, return True (go to next module) or False (fail, go to next event)"""
         objs = self.jetCollection(event)
         met = self.metInput(event)
         vectorSum = ROOT.TLorentzVector()
         scalarPtSum = 0.0
         for obj in objs:
-            vectorSum+=obj.p4()
-            scalarPtSum+=obj.pt
-        
-        self.out.fillBranch(self.outputName+"_ht",scalarPtSum)
-        setattr(event,self.outputName+"_ht",scalarPtSum)
-        self.out.fillBranch(self.outputName+"_mht",vectorSum.Pt())
-        setattr(event,self.outputName+"_mht",vectorSum.Pt())
-        self.out.fillBranch(self.outputName+"_mht_phi",vectorSum.Phi())
-        setattr(event,self.outputName+"_mht_phi",vectorSum.Phi())
-        self.out.fillBranch(self.outputName+"_mass",vectorSum.M())
-        setattr(event,self.outputName+"_mass",vectorSum.M())
+            vectorSum += obj.p4()
+            scalarPtSum += obj.pt
 
-        if self.leptonCollection!=None:
+        self.out.fillBranch(self.outputName+"_ht", scalarPtSum)
+        setattr(event, self.outputName+"_ht", scalarPtSum)
+        self.out.fillBranch(self.outputName+"_mht", vectorSum.Pt())
+        setattr(event, self.outputName+"_mht", vectorSum.Pt())
+        self.out.fillBranch(self.outputName+"_mht_phi", vectorSum.Phi())
+        setattr(event, self.outputName+"_mht_phi", vectorSum.Phi())
+        self.out.fillBranch(self.outputName+"_mass", vectorSum.M())
+        setattr(event, self.outputName+"_mass", vectorSum.M())
+
+        if self.leptonCollection is not None:
             met_px_lc = met.pt*math.sin(met.phi)
             met_py_lc = met.pt*math.cos(met.phi)
-            
-	    lepton = self.leptonCollection(event)
-            met_px_lc+=lepton.p4().Px()
-            met_py_lc+=lepton.p4().Py()
+
+        lepton = self.leptonCollection(event)
+            met_px_lc += lepton.p4().Px()
+            met_py_lc += lepton.p4().Py()
             met_lc = math.sqrt(met_px_lc**2+met_py_lc**2)
             self.out.fillBranch(self.outputName+"_MET_NoMu_pt", met_lc)
             setattr(event, self.outputName+"_MET_NoMu_pt", met_lc)
 
-	    met_l_mT = math.sqrt(2.*lepton.pt*met.pt*(1. - math.cos(lepton.phi - met.phi)))
-	    
-     	    self.out.fillBranch(self.outputName+"_MET_Mu_mT", met_l_mT)
-            setattr(event,self.outputName+"_MET_Mu_mT", met_l_mT)
+        met_l_mT = math.sqrt(2.*lepton.pt*met.pt*(1. - math.cos(lepton.phi - met.phi)))
 
-     	    self.out.fillBranch(self.outputName+"_mht_NoMu",vectorSum.Pt() + lepton.p4().Pt())
-            setattr(event,self.outputName+"_mht_NoMu",vectorSum.Pt() + lepton.p4().Pt())
-        
+        self.out.fillBranch(self.outputName+"_MET_Mu_mT", met_l_mT)
+            setattr(event, self.outputName+"_MET_Mu_mT", met_l_mT)
+
+        self.out.fillBranch(self.outputName+"_mht_NoMu", vectorSum.Pt() + lepton.p4().Pt())
+            setattr(event, self.outputName+"_mht_NoMu", vectorSum.Pt() + lepton.p4().Pt())
+
         minPhi = math.pi
         for obj in objs:
             negSum = -(vectorSum-obj.p4())
-            minPhi = min(minPhi,math.fabs(deltaPhi(negSum.Phi(),obj.phi)))
-        self.out.fillBranch(self.outputName+"_minPhi",minPhi)
-        setattr(event,self.outputName+"_minPhi",minPhi)
+            minPhi = min(minPhi, math.fabs(deltaPhi(negSum.Phi(), obj.phi)))
+        self.out.fillBranch(self.outputName+"_minPhi", minPhi)
+        setattr(event, self.outputName+"_minPhi", minPhi)
         return True
-        
